a23b902b7dc0ec879316a79075e37b66c66ab630
Antonio Romero Bravo <virtuald3@hotmail.com>
2024-08-08 21:05:26 +0200

    #00-SINTAXIS-PYTHON

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Magicometa.py
@@ -0,0 +1,43 @@
+# https://www.python.org/
+
+# Comentario de una linea.
+'Comentario de coma simple, en una linea.'
+"Comentario con comas dobles, en una linea."
+
+"""
+Comentario
+de varias
+líneas con 
+comas dobles.
+"""
+
+
+'''
+Comentario de varias
+lineas con comas simples
+'''
+
+# Creación de variable:
+
+variable_uno = "valor de la variable"
+variable_uno = "el valor puede cambiar"
+
+'''
+Python no tiene variables constantes,
+aunque si deseas una representación no real,
+se hace escribiéndola con mayúsculas
+'''
+
+CONSTANTE_IRREAL = "no debes de cambiar el valor"
+
+# Literales primitivos
+
+string = "Para cadenas de texto." 
+contatenacion_string = "[a-zA-Z0-9_]"
+entero = 5 #(int)
+flotante = 55.69 #(float)
+boleano = True #(true o false)
+
+# Imprimir en consola
+
+print("Hola, soy Python.")
